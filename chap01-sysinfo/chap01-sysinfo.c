11e36737d6997006581a86989a2a9554b10d504a
augusthan97 <69036097+augusthan97@users.noreply.github.com>
2021-02-17 15:03:23 +0900

    [update] chap01-sysinfo
    system information sizeof function

---
+++ chap01-sysinfo/chap01-sysinfo.c
@@ -0,0 +1,34 @@
+﻿/*
+* chap01-sysinfo.c
+* 시스템 정보확인
+* -----------------------------------------
+* sizeof(type 자료형) 내장함수(Built-in Function)
+* 자료형의 크기를 알 수 있는 함수
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf(">>> system information <<< \n");
+
+    printf("sizeof(char)(%d)\n", sizeof(char));
+    printf("sizeof(short)(%d)\n", sizeof(short));
+    printf("sizeof(int)(%d)\n", sizeof(int));
+    printf("sizeof(long)(%d)\n", sizeof(long));
+    printf("sizeof(float)(%d)\n", sizeof(float));
+    printf("sizeof(double)(%d)\n", sizeof(double));
+
+    printf("\n\n\n\n pointer sizeof\n");
+    printf("sizeof(char*)(%d)\n", sizeof(char*));
+    printf("sizeof(short*)(%d)\n", sizeof(short*));
+    printf("sizeof(int*)(%d)\n", sizeof(int*));
+    printf("sizeof(long*)(%d)\n", sizeof(long*));
+    printf("sizeof(float*)(%d)\n", sizeof(float*));
+    printf("sizeof(double*)(%d)\n", sizeof(double*));
+
+    printf("\n\n\n\n void pointer sizeof\n");
+    printf("sizeof(void*)(%d)\n", sizeof(void*));
+
+}
+
